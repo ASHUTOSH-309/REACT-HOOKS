b66f1c126f1b126a9a56ab2721a438b3e80bb7ae
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-12 20:04:44 +0530

    Both hard coded and fetched data rendered

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,55 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
-
- const [currentButton,setCurrentButton]=useState(1);
-
+  const [todos, setTodos] = useState([
+    {
+      title: "Breakfast",
+      description: "Angoor khauga"
+    },
+    {
+      title: "Lunch",
+      description: "Amity ka achcha mess"
+    },
+    {
+      title: "Dinner",
+      description: "Amrood khauga"
+    },
+    {
+      title: "Sleep",
+      description: "React Padhuga"
+    }
+  ])
+  const [currentButton, setCurrentButton] = useState(1);
 
   return (
     <>
-          <button onClick={()=>setCurrentButton(1)}>1</button>
-          <button onClick={()=>setCurrentButton(2)}>2</button>
-          <button onClick={()=>setCurrentButton(3)}>3</button>
-          <button onClick={()=>setCurrentButton(4)}>4</button>
-
-          <FetchTodo id={currentButton} />
+      <button onClick={() => setCurrentButton(1)}>1</button>
+      <button onClick={() => setCurrentButton(2)}>2</button>
+      <button onClick={() => setCurrentButton(3)}>3</button>
+      <button onClick={() => setCurrentButton(4)}>4</button>
+      <Todo title={todos[currentButton - 1].title} description={todos[currentButton - 1].description} />
+      <FetchTodo id={currentButton} />
     </>
   )
 }
 
+
+
 function FetchTodo({ id }) {
 
-  const [todos,setTodos]=useState({})
+  const [currentTodo, setCurrentTodo] = useState({})
 
-  useEffect(() => {
-    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).
-      then(function (response) {
-        setTodos(response.data.todo)
-      })
-  }, [id])
+    useEffect(() => {
+      axios.get("https://sum-server.100xdevs.com/todo?id=" + id).
+        then((response) => {
+          setCurrentTodo(response.data.todo)
+        })
+    }, [id])
 
   return <div>
-
-    <Todo key={todos.id} title={todos.title} description={todos.description} />
-
-    
+    <Todo title={currentTodo?.title} description={currentTodo?.description} />
   </div>
 }
-
 
 
 function Todo({ title, description }) {
